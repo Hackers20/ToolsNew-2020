c39daaaa0035336055c75cb8e966b4c314e2099f
Hackers20 <70922207+Hackers20@users.noreply.github.com>
2020-10-06 20:47:25 +0700

    Add files via upload

---
+++ Tools.sh
@@ -0,0 +1,81 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo "SABAR BRO LAGI LOADING...[\]" | lolcat
+sleep 1
+clear
+sleep 1
+echo "SELAMAT DATANG HACKER MAS BRO" | lolcat
+sleep 1
+clear
+sleep 1
+figlet TERMUX ID | lolcat
+sleep 1
+echo "<================================================>" | lolcat
+echo "<==========[    Tools By Hacker2    ]==========>" | lolcat
+echo "<=========[ Contact Me:6287778506999 ]==========>" | lolcat
+echo "<================================================>" | lolcat
+echo
+echo
+sleep 2
+echo $cyan"================================="
+echo $green"1).Hack Akun Facebook Target"
+echo $cyan"================================="
+sleep 2
+echo $cyan"================================="
+echo $green"2).Hack Pulsa"
+echo $cyan"================================="
+sleep 2
+echo $cyan"================================="
+echo $green"3).Checker ML"
+echo $cyan"================================="
+sleep 2
+echo $cyan"================================="
+echo $green"4).Tombol Termux"
+echo $cyan"================================="
+sleep 2
+echo $cyan"================================="
+echo $green"5).BRUTERFORCH"
+echo $cyan"================================="
+sleep 2
+echo $cyan"================================="
+echo $green"6).Sadap WA"
+echo $cyan"================================="
+sleep 2
+echo $cyan"================================="
+echo $green"7).Diamond Gratis"
+echo $cyan"================================="
+sleep 2
+echo $cyan"================================="
+echo $green"8).Virus Target"
+echo $cyan"================================="
+sleep 2
+echo $cyan"================================="
+echo $green"9).Spam WA"
+echo $cyan"================================="
+sleep 2
+echo $cyan"================================="
+echo $green"10).Install Bahan"
+echo $cyan"================================="
+sleep 2
+echo $cyan"================================="
+echo $green"00).KELUAR DARI TOOLS/EXIT"
+echo $cyan"================================="
+echo
+sleep 3
+read -p $blue"Hack>> " : pil
+
+if [ $pil = 01 ] || [ $pil = 1 ]
+then
+git clone https://github.com/Hackers20/Hack-Facebook-Target
+cd Hack-Facebook-Target
+sh Hack.sh
+fi
+clear
+sh Tools.sh
