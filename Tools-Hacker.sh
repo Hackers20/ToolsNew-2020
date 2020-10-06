edcbb36e81a0d208e8b15101fdcfc09cf446eb9d
Hackers20 <70922207+Hackers20@users.noreply.github.com>
2020-10-06 20:48:35 +0700

    Add files via upload

---
+++ Tools-Hacker.sh
@@ -0,0 +1,241 @@
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 3
+echo "SABAR BRO LAGI LOADING...[\]" | lolcat
+sleep 3
+clear
+sleep 3
+echo "SELAMAT DATANG BRO" | lolcat
+sleep 3
+clear
+sleep 1
+toilet -f big -F gay TOOlS
+toilet -f big -F gay HACKER
+sleep 2
+echo
+sleep 1
+echo "<================================================>" | lolcat
+echo "<==========[    Tools By Hacker2    ]==========>" | lolcat
+echo "<=========[ Contact Me:6287778506999 ]==========>" | lolcat
+echo "<================================================>" | lolcat
+echo
+sleep 1
+echo "Pilih Tools [•]" | lolcat
+sleep 1
+echo "=================================" | lolcat
+echo "1). Hack Diamond" | lolcat
+echo "=================================" | lolcat
+echo "2). Crack Fb" | lolcat
+echo "=================================" | lolcat
+echo "3). Virus Target" | lolcat
+echo "=================================" | lolcat
+echo "4). Hack Facebook Target" | lolcat
+echo "=================================" | lolcat
+echo "4). Tombol Termux" | lolcat
+echo "=================================" | lolcat
+echo "5). Hack Pulsa" | lolcat
+echo "=================================" | lolcat
+echo "6). Follower IG" | lolcat
+echo "=================================" | lolcat
+echo "7). BRUTERFORC 2020" | lolcat
+echo "=================================" | lolcat
+echo "8). Spam WA" | lolcat
+echo "=================================" | lolcat
+echo "9). Sadap WA" | lolcat
+echo "=================================" | lolcat
+echo "10). Tools Installer" | lolcat
+echo "=================================" | lolcat
+echo "11). Tools Hacker" | lolcat
+echo "=================================" | lolcat
+echo "12). Checker Moonton" | lolcat
+echo "=================================" | lolcat
+echo "13). Kereta Api Termux" | lolcat
+echo "=================================" | lolcat
+echo "14). Teks Berjalan" | lolcat
+echo "=================================" | lolcat
+echo "15). Install Bahan" | lolcat
+echo "=================================" | lolcat
+echo "00). Exit The Tools" | lolcat
+echo "=================================" | lolcat
+echo
+echo "╭─Hack20" |lolcat
+read -p "╰─Hack> " pil;
+echo
+sleep 1
+echo
+if [ $pil = 01 ] || [ $pil = 1 ]
+then
+clear
+
+git clone https://github.com/Hackers20/Diamond-Gratis
+cd Diamond-Gratis
+sh diamondfree.sh
+fi
+
+
+if [ $pil = 02 ] || [ $pil = 2 ]
+then
+clear
+
+git clone https://github.com/Hackers20/Crack-Fb
+cd Crack-Fb
+sh Dark.sh
+fi
+
+
+if [ $pil = 03 ] || [ $pil = 3 ]
+then
+clear
+
+git clone https://github.com/Hackers20/Virus-Target66
+cd Virus-Target66
+sh Virus.sh
+fi
+
+
+if [ $pil = 04 ] || [ $pil = 4 ]
+then
+clear
+
+git clone https://github.com/Hackers20/Hack-Facebook-Target
+cd Hack-Facebook-Target
+sh Hack.sh
+fi
+
+
+if [ $pil = 05 ] || [ $pil = 5 ]
+then
+clear
+
+git clone https://github.com/Hackers20/Hack-Pulsa2020
+cd Hack-Pulsa2020
+sh puls4.sh
+fi
+
+
+if [ $pil = 06 ] || [ $pil = 6 ]
+then
+clear
+
+git clone https://github.com/Hackers20/Follower-IGNew
+cd Follower-IGNew
+sh IG.sh
+fi
+
+
+if [ $pil = 07 ] || [ $pil = 7 ]
+then
+clear
+
+git clone https://github.com/Hackers20/BRUTEFORC-2020
+cd BRUTERFORC-2020
+sh BRUTERFORC.sh
+fi
+
+
+if [ $pil = 08 ] || [ $pil = 8 ]
+then
+clear
+
+git clone https://github.com/Hackers20/Spam-Wa2020
+cd Spam-Wa2020
+php spamwa.php
+fi
+
+
+if [ $pil = 09 ] || [ $pil = 9 ]
+then
+clear
+
+git clone https://github.com/Hackers20/Sadap-WA
+cd Sadap-WA
+sh sadapwa.sh
+fi
+
+
+if [ $pil = 010 ] || [ $pil = 10 ]
+then
+clear
+
+git clone https://github.com/Hackers20/Tools-Installer
+cd Tools-Installer
+sh ToolsInstaller.sh
+fi
+
+
+if [ $pil = 011 ] || [ $pil = 11 ]
+then
+clear
+
+git clone https://github.com/Hackers20/Tools-Hacker
+cd Tools-Hacker
+sh Tools.sh
+fi
+
+
+if [ $pil = 012 ] || [ $pil = 12 ]
+then
+clear
+
+git clone https://github.com/dz-id/MoontonChecker
+cd Moonton-Checker
+python moonton.py
+fi
+
+
+if [ $pil = 013 ] || [ $pil = 13 ]
+then
+cleat
+
+sl
+fi
+
+
+if [ $pil = 014 ] || [ $pil = 14 ]
+then
+clear
+
+cmatrix
+fi
+
+
+if [ $pil = 015 ] || [ $pil = 15 ]
+then
+clear
+
+pkg update && pkg upgrade
+pkg install python
+pkg install git
+pkg install figlet
+pkg install toilet
+pkg install lolcat
+pkg install ruby
+gem install lolcat
+pip install requests
+pip2 install mechanize
+pip install requests futures bs4
+pkg install nano
+pkg install sl
+pkg install cmatrix
+fi
+
+
+if [ $pil = 000 ] || [ $pil = 00 ]
+then
+clear
+
+figlet EXIT | lolcat
+echo "================================================" | lolcat
+echo "Bye don't use this tools wrong" | lolcat
+echo "don't forget to subscribe to my channel" | lolcat
+echo "if there is an error please contact the admin" | lolcat
+echo "================================================" | lolcat
+fi
+exit
